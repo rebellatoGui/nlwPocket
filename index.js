c75df04ac057f920ee77b3ba5103c4758d352caf
Gui <guilhermerebellato7@gmail.com>
2024-09-11 21:15:58 -0300

    Implementing: metas abertas

--- index.js
+++ index.js
@@ -57,8 +57,24 @@ async function metasRealizadas() {
   }
 
   await select({
-    message: "Metas Realizdas",
+    message: "Metas Realizdas:" + realizadas.length,
     choices: [...realizadas],
+  });
+}
+
+async function metasAbertas() {
+  const abertas = metas.filter((meta) => {
+    return meta.checked != true;
+  });
+
+  if (abertas.length == 0) {
+    console.log("Não existem metas abertas! =] ");
+    return;
+  }
+
+  await select({
+    message: "Metas Abertas:" + abertas.length,
+    choices: [...abertas],
   });
 }
 
@@ -80,6 +96,10 @@ async function start() {
           value: "realizadas",
         },
         {
+          name: "Metas abertas",
+          value: "abertas",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
@@ -95,6 +115,9 @@ async function start() {
         break;
       case "realizadas":
         await metasRealizadas();
+        break;
+      case "abertas":
+        await metasAbertas();
         break;
       case "sair":
         console.log("Até a próxima!");
